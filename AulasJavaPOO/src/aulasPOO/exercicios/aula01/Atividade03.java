624fde35346dbd1385e47f04f7be9f36e896b4ec
diegodiaaz <diego.reis.ruiz@gmail.com>
2023-06-07 18:21:17 -0300

    Revisão laços de repetição, soma de 0 à 100

---
+++ AulasJavaPOO/src/aulasPOO/exercicios/aula01/Atividade03.java
@@ -0,0 +1,21 @@
+package aulasPOO.exercicios.aula01;
+
+public class Atividade03 {
+
+	public static void main(String[] args) {
+
+		int i = 0;
+		int total = 0;
+		
+		while (i <= 100) {
+			
+			total += i;
+			i++;
+			
+		}
+		
+		System.out.println("O total da soma de zero até 100 é: " + total);
+
+	}
+
+}
