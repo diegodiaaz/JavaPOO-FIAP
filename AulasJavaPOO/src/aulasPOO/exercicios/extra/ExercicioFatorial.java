e1acc8749f9bee4838f690cd54fef89095a3377f
logonrmlocal <logonrmlocal@PA603MICRO26.fiap.com.br>
2023-06-12 15:47:50 -0300

    Realizando exercícios extras em monitoria

---
+++ AulasJavaPOO/src/aulasPOO/exercicios/extra/ExercicioFatorial.java
@@ -0,0 +1,21 @@
+package aulasPOO.exercicios.extra;
+
+import javax.swing.JOptionPane;
+
+public class ExercicioFatorial {
+
+	public static void main(String[] args) {
+
+		float num, fatoracao = 1, a;
+
+		num = Float.parseFloat(JOptionPane.showInputDialog("Insira o número que você deseja fatorar: "));
+
+		for (a = 1; a <= num; a++) {
+			fatoracao = fatoracao * a;
+		}
+		
+		JOptionPane.showMessageDialog(null, String.format(" O fatorial de %.0f é:  %.0f", num, fatoracao));
+
+	}
+
+}
---
+++ AulasJavaPOO/src/aulasPOO/exercicios/extra/ExercicioFibonacci.java
@@ -0,0 +1,10 @@
+package aulasPOO.exercicios.extra;
+
+public class ExercicioFibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
